0ed12fddc3e0023c609b516f424dcba1a80c837a
PengfeiWeng <pengfeiweng@Pengfeis-MacBook-Pro-2.local>
2016-11-21 00:33:52 -0800

    assignment2

---
+++ assignment2/app.js
@@ -0,0 +1,47 @@
+(function (){
+  'use strict';
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+
+  function ToBuyController(ShoppingListCheckOffService) {
+    var ctrl1 = this;
+    ctrl1.toBuyItems = ShoppingListCheckOffService.getToBuyItems();
+
+    ctrl1.buyItem = function (index) {
+      ShoppingListCheckOffService.buyItem(index);
+    };
+  }
+
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var ctrl2 = this;
+    ctrl2.boughtItems = ShoppingListCheckOffService.getBoughtItems();
+  }
+
+  function ShoppingListCheckOffService() {
+    this.toBuyItems = [
+      { name: "cookies", quantity: 10 },
+      { name: "dumplings", quantity: 5 },
+      { name: "chips", quantity: 4 },
+    ];
+
+    this.boughtItems = [];
+
+    this.getToBuyItems = function() {
+      return this.toBuyItems;
+    };
+
+    this.getBoughtItems = function () {
+      return this.boughtItems;
+    };
+
+    this.buyItem = function (index) {
+      var item = this.toBuyItems.splice(index, 1);
+      this.boughtItems.push(item[0]);
+    }
+  }
+})();
